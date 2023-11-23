eb4a9891a672681d683c3fbef8fe5a137880b542
Davide Chiozzi <davidechiozzi@Computer-portatile-di-Davide.local>
2023-11-23 15:55:41 +0700

    clean

--- src/services/api/api.config.ts
+++ src/services/api/api.config.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
  export const BASE_URL = 'http://localhost:3000';
-// export const BASE_URL = '/api/';
-export const MOCK_BASE_URL = `${BASE_URL}`;
 
 
 export const API = axios.create({
@@ -10,18 +8,3 @@ export const API = axios.create({
   timeout: 60_000, // In milliseconds
 });
 
-// export default APIks;
-
-// import axios from 'axios';
-
-// export function createAxiosClient({options, getAuthToken, logout}){
-//   const client = axios.create(options);
-
-//   client.interceptors.request.use(
-//     (config) =>{
-//       if (config.authorization !== false) {
-
-//       }
-//     }
-//   )
-// }
